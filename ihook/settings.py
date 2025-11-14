c1c53c1d028b1afe16226f050d6094fb07ffad7b
Lorenzo Diel <lorenzodiel5@gmail.com>
2025-11-14 16:11:41 -0300

    Config Statics

--- ihook/settings.py
+++ ihook/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"   # render servirá estos archivos
-STATICFILES_DIRS = [BASE_DIR / "web" / "static"]  # si ya tenés static en web/static
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")   # render servirá estos archivos
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "web", "static")]  # si ya tenés static en web/static
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
